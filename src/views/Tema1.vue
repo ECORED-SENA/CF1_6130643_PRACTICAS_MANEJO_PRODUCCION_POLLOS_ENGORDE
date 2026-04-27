145ad45b07e0ece6ab5009fd1da558cacdd63979
Henry Alvarez <halvarezast@gmail.com>
2026-04-27 06:55:44 -0500

    Update Youtube

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -434,7 +434,7 @@
     //- Inicio Video
     figure.mb-5(data-aos="fade-down")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/nHjl83QvSUw?si=PYOSxqLdAk88V5nb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/TFDKnzGlVPI?si=tg-Fz2Evai3Tl6F2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     //- Fin Video
     Separador
     //- Inicio Tema1.4
